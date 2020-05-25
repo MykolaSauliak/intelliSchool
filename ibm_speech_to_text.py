26899131a04fe05860da5a32d7c04b649480c578
Priya Darshini <msvdpriya@gmail.com>
2020-05-25 02:01:10 -0400

    removed api key

--- ibm_speech_to_text.py
+++ ibm_speech_to_text.py
@@ -6,9 +6,9 @@ from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 import srtMaker
 
 
-authenticator = IAMAuthenticator('wDAb08LCoNUTYoF7nsrOO7h_nBHB_XZuHRnu5Ll0ybOH')
+authenticator = IAMAuthenticator('XXXXXX')
 service = SpeechToTextV1(authenticator=authenticator)
-service.set_service_url('https://api.us-east.speech-to-text.watson.cloud.ibm.com/instances/1ffa4e08-9589-4ab6-8c9d-af4abca6bd4e')
+service.set_service_url('XXXX')
 
 #audio is to be extracted as wav using ffmpeg(***very important)
 def get_transcript(audioFilePath):

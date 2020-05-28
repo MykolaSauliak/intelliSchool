df18e4584cd01446a74343d05f0d8edb6ec13abe
Priya Darshini <msvdpriya@gmail.com>
2020-05-28 12:00:31 -0400

    Update redis_utilities.py

--- redis_utilities.py
+++ redis_utilities.py
@@ -22,10 +22,8 @@ def get_json(id):
 def file_exist(dictionary_name,key):
     return r.hexists(dictionary_name,key)
 
-def add_file_to_stream(dictionary_name,stream_name,video_id,value):
-    stream_id = r.xadd(stream_name, {'Summary': str(value).encode()})
-    add_dict(dictionary_name,stream_id,str(video_id).encode())
+def add_file_to_stream(stream_name,key,value):
+    return r.xadd(stream_name, {key: str(value).encode()})
     
-def get_file_from_stream(stream_name,video_id):
-    val = r.xrange(stream_name, min=(r.hget("map",video_id)), max=(r.hget("map",'_5OvgQW6FG4')), count=None)
-    for k,v in dict(val).items():return (v[b'Value'])
+def get_file_from_stream(stream_name,stream_id):
+    return r.xrange(stream_name, min=stream_id, max=stream_id, count=None)

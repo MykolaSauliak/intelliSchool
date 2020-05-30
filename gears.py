45227d3b78dcc50e87b3e20669fbb1d11c31f852
Sumanth M <sumanthpdm@gmail.com>
2020-05-31 00:38:40 +0530

    Update gears.py

--- gears.py
+++ gears.py
@@ -11,27 +11,42 @@ generator_stream = "Generator_Stream"
 text_stream = 'Text_Stream'
 VTT_stream = 'VTT_Stream'
 
+def xlog(*args):
+    redisgears.executeCommand('xadd', 'log', '*', 'text', ' '.join(map(str, args)))
+    
 def store_transcribed_results(x):
-    video_id = x['video_id']
-    fragment_counter = redis_utilities.get_dict(video_id)
-    redis_utilities.add_dict(video_id, fragment_counter - 1)
-    execute('XADD', transcribed_stream, 'MAXLEN', '~', 1000, '*', 'video_id': video_id, 'fragment_id', x['fragment_id'], 'trans_text', x['trans_text'])
-    if fragment_counter == 1:
-        generator_gear(video_id)
-    return video_id
+    try:
+        video_id = x['video_id']
+        fragment_counter = redis_utilities.get_dict(video_id)
+        redis_utilities.add_dict(video_id, fragment_counter - 1)
+        execute('XADD', transcribed_stream, 'MAXLEN', '~', 1000, '*', 'video_id': video_id, 'fragment_id', x['fragment_id'], 'trans_text', x['trans_text'])
+        if fragment_counter == 1:
+            generator_gear(video_id)
+        return video_id
+    except:
+        xlog('store_generated_results: error:', sys.exc_info()[0])
 
 def speech_to_text(x):
-    trans_text = ibm_speech_to_text.transcribe(x['fragment'])
-    return x['video_id'], x['fragment_id'], trans_text
+    try:
+        trans_text = ibm_speech_to_text.transcribe(x['fragment'])
+        return x['video_id'], x['fragment_id'], trans_text
+    except:
+        xlog('store_generated_results: error:', sys.exc_info()[0])
 
 def store_generated_results(x):
-    video_id = x['video_id']
-    redis_utilities.add_json(video_id, x['data'])
-    redis_utilities.publish('status', common.Status.COMPLETE)
-    return video_id
+    try:
+        video_id = x['video_id']
+        redis_utilities.add_json(video_id, x['data'])
+        redis_utilities.publish('status', common.Status.COMPLETE)
+        return video_id
+    except:
+         xlog('store_generated_results: error:', sys.exc_info()[0])
 
 def _generate_quiz(x):
-    return generate_quiz(x['data'])
+    try:
+        return generate_quiz(x['data'])
+    except:
+        xlog('generate_quiz: error:', sys.exc_info()[0])
 
 def generator_gear():
     video_id = video_id
